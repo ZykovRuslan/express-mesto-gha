3a5d5a3c795f746334d737746064e00fe5c9913a
Ruslan Zykov <r.v.zykov@rambler.ru>
2023-06-13 16:36:42 +0400

    fix

--- routes/cards.js
+++ routes/cards.js
@@ -1,12 +1,18 @@
-import { Router } from 'express';
-import { getCards, deleteCard, createCard, likeCard, dislikeCard } from '../controllers/cards';
+import { Router } from "express";
+import {
+  getCards,
+  deleteCard,
+  createCard,
+  likeCard,
+  dislikeCard,
+} from "../controllers/cards";
 
 const cardsRouter = Router();
 
-cardsRouter.get('/', getCards);
-cardsRouter.delete('/:cardId', deleteCard);
-cardsRouter.post('/', createCard);
-cardsRouter.put('/:cardId/likes', likeCard);
-cardsRouter.delete('/:cardId/likes', dislikeCard);
+cardsRouter.get("/", getCards);
+cardsRouter.delete("/:cardId", deleteCard);
+cardsRouter.post("/", createCard);
+cardsRouter.put("/:cardId/likes", likeCard);
+cardsRouter.delete("/:cardId/likes", dislikeCard);
 
 export default cardsRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,18 @@
-import { Router } from 'express';
-import { getUsers, getUserById, createUser, updateUser, updateAvatar } from '../controllers/users';
+import { Router } from "express";
+import {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  updateAvatar,
+} from "../controllers/users";
 
 const usersRouter = Router();
 
-usersRouter.get('/', getUsers);
-usersRouter.get('/:userId', getUserById);
-usersRouter.post('/', createUser);
-usersRouter.patch('/me', updateUser);
-usersRouter.patch('/me/avatar', updateAvatar);
+usersRouter.get("/", getUsers);
+usersRouter.get("/:userId", getUserById);
+usersRouter.post("/", createUser);
+usersRouter.patch("/me", updateUser);
+usersRouter.patch("/me/avatar", updateAvatar);
 
 export default usersRouter;
