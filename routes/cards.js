e04eb923966cac8e5980939035339b6c29471eb8
Ruslan Zykov <r.v.zykov@rambler.ru>
2023-06-13 16:42:19 +0400

    fix

--- routes/cards.js
+++ routes/cards.js
@@ -1,18 +1,18 @@
-import { Router } from "express";
+import { Router } from 'express';
 import {
   getCards,
   deleteCard,
   createCard,
   likeCard,
   dislikeCard,
-} from "../controllers/cards";
+} from '../controllers/cards';
 
 const cardsRouter = Router();
 
-cardsRouter.get("/", getCards);
-cardsRouter.delete("/:cardId", deleteCard);
-cardsRouter.post("/", createCard);
-cardsRouter.put("/:cardId/likes", likeCard);
-cardsRouter.delete("/:cardId/likes", dislikeCard);
+cardsRouter.get('/', getCards);
+cardsRouter.delete('/:cardId', deleteCard);
+cardsRouter.post('/', createCard);
+cardsRouter.put('/:cardId/likes', likeCard);
+cardsRouter.delete('/:cardId/likes', dislikeCard);
 
 export default cardsRouter;
