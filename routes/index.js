b4c7b865f053bc9c9a5dd885a3899a7d733cc5fd
Ruslan Zykov <r.v.zykov@rambler.ru>
2023-06-16 15:46:34 +0400

    refactor: роут для всех некорректных запросов

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
 
+router.all('*', (req, res) => {
+  res.send('Неверный путь');
+});
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);
 
